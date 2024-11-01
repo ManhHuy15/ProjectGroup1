91eea27156ff53315995973b84a056a0abfc91d2
kttanh <kttanh03@gmail.com>
2024-11-01 23:44:45 +0700

    ...

--- src/main/java/dbcontext/DBContext.java
+++ src/main/java/dbcontext/DBContext.java
@@ -9,8 +9,8 @@ public class DBContext {
     public DBContext() {
         try {
             String url = "jdbc:sqlserver://localhost:1433;databaseName=assignmentPRJ301;trustServerCertificate=true";
-            String username = "sa";
-            String password = "123";
+            String username = "kttanh";
+            String password = "2003";
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
